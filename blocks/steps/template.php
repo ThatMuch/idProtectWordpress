c1ab73abb2b28595b7f10c6a3830812bd6be97f1
Mathilde Arconte <signup@marconte.fr>
2023-03-26 17:09:03 +0200

    description steps

--- blocks/steps/template.php
+++ blocks/steps/template.php
@@ -21,38 +21,40 @@ $class_name = $args['class_name'];
 <div class="row g-5 align-items-center">
 	<div class="col-lg-5">
 		<?php $image = $data['image'];  ?>
-			<?php if ( !empty( $image ) ) : ?>
-					<img
-					src="<?php echo esc_url($image['url']); ?>"
-					alt="<?php echo esc_attr($image['alt']); ?>" />
-			<?php endif; ?>
+		<?php if (!empty($image)) : ?>
+			<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+		<?php endif; ?>
 	</div>
 	<div class="col-lg-7">
 		<div class="step__list">
 
-<!-- display the list of elements from a repeater field in acf -->
-			<?php if( have_rows('steps') ): ?>
+			<!-- display the list of elements from a repeater field in acf -->
+			<?php if (have_rows('steps')) : ?>
 				<ul class="nav nav-tabs">
 					<?php $i = 1; ?>
-					<?php while( have_rows('steps') ): the_row();
+					<?php while (have_rows('steps')) : the_row();
 						// vars
 						$step = get_sub_field('step');
 						$step_number = get_sub_field('step_number');
 						$step_title = get_sub_field('title');
 						$step_description = get_sub_field('description');
-						?>
+					?>
 						<li>
 							<a data-bs-toggle="tab" href="#step-<?php echo $i; ?>">
 								<span class="text">
 									<span class="g-text"><?php echo $i; ?></span>
 								</span>
-								<?php echo $step_title; ?>
-								<?php if ($step_description) {
-									echo $step_description;
-								}; ?>
+								<div class="text__body">
+									<span class="head"> <?php echo $step_title; ?></span>
+									<?php if ($step_description) {
+										echo $step_description;
+									}; ?>
+
+								</div>
 							</a>
 						</li>
-					<?php $i++; endwhile; ?>
+					<?php $i++;
+					endwhile; ?>
 				</ul>
 			<?php endif; ?>
 		</div>
