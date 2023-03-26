71fdf19a8409921f1ae4b7f5711178ff5ea5526f
Mathilde Arconte <signup@marconte.fr>
2023-03-26 16:22:38 +0200

    fix card text box

--- blocks/serviceSection1/template.php
+++ blocks/serviceSection1/template.php
@@ -12,50 +12,52 @@ $block_id = $args['block_id'];
 $class_name = $args['class_name'];
 ?>
 
-<div class="row g-3 align-items-end">
-<?php if ( have_rows( 'card_1' ) ) : ?>
-	<?php while ( have_rows( 'card_1' ) ) : the_row(); ?>
-	    <div class="col-xl-4 col-lg-6">
-			<div class="card__text__box">
-				<?php $image = get_sub_field('image');  ?>
-					<?php if ( !empty( $image ) ) : ?>
-						<img
-						src="<?php echo esc_url($image['url']); ?>"
-						alt="<?php echo esc_attr($image['alt']); ?>" />
+<div class="row g-3 align-items-end mb-5
+<?php if (have_rows('card_2')) {
+	while (have_rows('card_2')) {
+		the_row();
+		if (empty(get_sub_field('image'))) {
+			echo 'align-items-stretch';
+		}
+	}
+}; ?>">
+	<?php if (have_rows('card_1')) : ?>
+		<?php while (have_rows('card_1')) : the_row(); ?>
+			<div class="col-xl-4 col-lg-6">
+				<div class="card__text__box">
+					<?php $image = get_sub_field('image');  ?>
+					<?php if (!empty($image)) : ?>
+						<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 					<?php endif; ?>
-				<h2 class="mt-5"><?php echo get_sub_field('title'); ?></h2>
-				<h4><?php echo get_sub_field('text'); ?></h4>
-				<!-- get link -->
-				<?php $link = get_sub_field('llink'); ?>
-				<?php if ( $link ) : ?>
-					<a
-					class="btn btn__white"
-					href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>"><?php echo esc_html( $link['title'] ); ?>
-				<img src="<?php echo get_template_directory_uri()?>/assets/images/arrow-right.svg" alt="Flèche vers la droite">
-				</a>
-				<?php endif; ?>
+					<h2 class="mt-5"><?php echo get_sub_field('title'); ?></h2>
+					<h4><?php echo get_sub_field('text'); ?></h4>
+					<!-- get link -->
+					<?php $link = get_sub_field('llink'); ?>
+					<?php if ($link) : ?>
+						<a class="btn btn__white" href="<?php echo esc_url($link['url']); ?>" target="<?php echo esc_attr($link['target']); ?>"><?php echo esc_html($link['title']); ?>
+							<img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow-right.svg" alt="Flèche vers la droite">
+						</a>
+					<?php endif; ?>
+				</div>
 			</div>
-        </div>
-	<?php endwhile; ?>
-<?php endif; ?>
+		<?php endwhile; ?>
+	<?php endif; ?>
 
-<?php if(have_rows('card_2')) : ?>
-	<?php while (have_rows('card_2')) : the_row(); ?>
-		<div class="col-xl-8 col-lg-6">
-			<div class="card__text__image">
+	<?php if (have_rows('card_2')) : ?>
+		<?php while (have_rows('card_2')) : the_row(); ?>
+			<div class="col-xl-8 col-lg-6">
 				<?php $image = get_sub_field('image');  ?>
-					<?php if ( !empty( $image ) ) : ?>
-						<img
-						src="<?php echo esc_url($image['url']); ?>"
-						alt="<?php echo esc_attr($image['alt']); ?>" />
-					<?php endif; ?>
+				<?php if (!empty($image)) : ?>
+					<div class="card__text__image">
+						<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+					</div>
+				<?php endif; ?>
+				<div class="card__text__body">
+					<?php echo get_sub_field('title'); ?>
+					<p><?php echo get_sub_field('text'); ?></p>
+				</div>
 			</div>
-			<div class="card__text__body">
-				<?php echo get_sub_field('title'); ?>
-				<p><?php echo get_sub_field('text'); ?></p>
-			</div>
-		</div>
-	<?php endwhile; ?>
-<?php endif; ?>
+		<?php endwhile; ?>
+	<?php endif; ?>
 
 </div>
