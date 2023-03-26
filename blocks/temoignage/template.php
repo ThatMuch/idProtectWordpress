011fb0e366583ab5c72a66986f2730afcabae349
Mathilde Arconte <signup@marconte.fr>
2023-03-26 20:32:06 +0200

    fix link to article

--- blocks/temoignage/template.php
+++ blocks/temoignage/template.php
@@ -19,42 +19,37 @@ $class_name = $args['class_name'];
 				<div class="testimonial">
 					<div class="testimonial__right order-md-1">
 						<h4 class="title text__orange"><?php echo $data['title']; ?></h4>
-							<?php if ( have_rows( 'video_preview' ) ) : ?>
-								<?php while ( have_rows( 'video_preview' ) ) : the_row(); ?>
-									<?php $image_1 = get_sub_field( 'image_2' ); ?>
-									<?php if ( $image_1 ) : ?>
-										<img src="<?php echo esc_url( $image_1['url'] ); ?>" alt="<?php echo esc_attr( $image_1['alt'] ); ?>" />
-									<?php endif; ?>
-								<?php endwhile; ?>
-							<?php endif; ?>
+						<?php if (have_rows('video_preview')) : ?>
+							<?php while (have_rows('video_preview')) : the_row(); ?>
+								<?php $image_1 = get_sub_field('image_2'); ?>
+								<?php if ($image_1) : ?>
+									<img src="<?php echo esc_url($image_1['url']); ?>" alt="<?php echo esc_attr($image_1['alt']); ?>" />
+								<?php endif; ?>
+							<?php endwhile; ?>
+						<?php endif; ?>
 					</div>
 					<div class="testimonial__left order-md-0">
-						<?php if ( have_rows( 'video_preview' ) ) : ?>
-							<?php while ( have_rows( 'video_preview' ) ) : the_row(); ?>
-								<?php $image_2 = get_sub_field( 'image_1' ); ?>
-								<?php if ( $image_2 ) : ?>
-									<img src="<?php echo esc_url( $image_2['url'] ); ?>" alt="<?php echo esc_attr( $image_2['alt'] ); ?>" />
+						<?php if (have_rows('video_preview')) : ?>
+							<?php while (have_rows('video_preview')) : the_row(); ?>
+								<?php $image_2 = get_sub_field('image_1'); ?>
+								<?php if ($image_2) : ?>
+									<img src="<?php echo esc_url($image_2['url']); ?>" alt="<?php echo esc_attr($image_2['alt']); ?>" />
 
 								<?php endif; ?>
 							<?php endwhile; ?>
 						<?php endif; ?>
-						<button type="button" data-bs-toggle="modal" data-bs-target="#videoModal"><img src="<?php echo get_template_directory_uri()?>/assets/images/arrow-right-white.svg" alt="Flèche vers la droite"></button>
+						<button type="button" data-bs-toggle="modal" data-bs-target="#videoModal"><img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow-right-white.svg" alt="Flèche vers la droite"></button>
 
 					</div>
 				</div>
 			</div>
 			<div class="col-lg-5">
 				<div class="testimonial__details">
-					<?php echo $data['subtitle'];?>
-					<p><?php echo $data['description'];?></p>
-					<a href="#" class="btn btn__orange">voir l'article</a>
-
+					<?php echo $data['subtitle']; ?>
+					<p><?php echo $data['description']; ?></p>
+					<a href="<?php echo $data['link']; ?>" class="btn btn__orange">voir l'article</a>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
-
-
-
-
