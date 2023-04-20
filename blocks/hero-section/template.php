6f64e806299a062d207e401974ad23fbb11b6b6a
Mathilde Arconte <signup@marconte.fr>
2023-04-20 20:39:39 +0200

    update hero section

--- blocks/hero-section/template.php
+++ blocks/hero-section/template.php
@@ -5,14 +5,9 @@ $image = get_field('image');
 $description = get_field('description');
 ?>
 <div class="hero__area">
-	<?php if ($image) : ?>
-		<div class="hero__image">
-			<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
-		</div>
-	<?php endif; ?>
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-9">
+			<div class="col-lg-7">
 				<div class="hero__text">
 					<?php echo $title; ?>
 					<div class="hero__box">
@@ -28,13 +23,20 @@ $description = get_field('description');
 									$cta_primary = get_sub_field('primary_cta');
 									$cta_secondary = get_sub_field('secondary_cta');
 								?>
-									<a href="<?php echo esc_url($cta_primary['url']); ?>" target="<?php echo esc_attr($cta_primary['target']); ?>"><span><?php echo esc_html($cta_primary['title']); ?></span><img src="<?php echo get_template_directory_uri() ?>/assets/images/icon-1.svg" alt="Flèche vers la droite"></a>
-									<a href="<?php echo esc_url($cta_secondary['url']); ?>" target="<?php echo esc_attr($cta_secondary['target']); ?>"><span><?php echo esc_html($cta_secondary['title']); ?></span><img src="<?php echo get_template_directory_uri() ?>/assets/images/icon-2.svg" alt="Flèche vers la droite"></a>
+									<a href="<?php echo esc_url($cta_primary['url']); ?>" target="<?php echo esc_attr($cta_primary['target']); ?>"><span><?php echo esc_html($cta_primary['title']); ?></span><img src="<?php echo get_template_directory_uri() ?>/assets/images/icon-1.svg" alt="Particulier"></a>
+									<a href="<?php echo esc_url($cta_secondary['url']); ?>" target="<?php echo esc_attr($cta_secondary['target']); ?>"><span><?php echo esc_html($cta_secondary['title']); ?></span><img src="<?php echo get_template_directory_uri() ?>/assets/images/icon-2.svg" alt="Professionnel"></a>
 								<?php endwhile; ?>
 							</div>
 						<?php endif; ?>
 					</div>
 				</div>
+			</div>
+			<div class="col-lg-5 d-flex">
+				<?php if ($image) : ?>
+					<div class="hero__image mt-auto">
+						<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+					</div>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
