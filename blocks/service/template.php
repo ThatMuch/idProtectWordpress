7b721e408166be4f16225549679b5448869d2490
Mathilde Arconte <signup@marconte.fr>
2023-05-15 15:42:12 +0200

    Titre des sections sur mobile

--- blocks/service/template.php
+++ blocks/service/template.php
@@ -13,26 +13,29 @@ $class_name = $args['class_name'];
 ?>
 <div class="container">
 	<div class="row">
-<div class="col-lg-8 mx-auto">
-	<div id="<?php echo $block_id; ?>" class="<?php echo $class_name; ?>">
-			<?php if ( $data['image'] ) : ?>
-			<div class="card__icon">
-			<img src="<?php echo esc_url( $data['image']['url'] ); ?>" alt="<?php echo esc_attr( $data['image']['alt'] ); ?>" />
-			</div>
-			<?php endif; ?>
-			<div class="card__text">
-				<h3 class="title"><span class="g-text"><?php echo $data['title']; ?></span></h3>
-				<div class="card__text__box mh-auto bg_<?php echo $args['bg_color'];?>">
-					<h3><?php echo $data['subtitle']; ?></h3>
-					<p><?php echo $data['description'];?></p>
-					<?php if ( $data['link'] ) : ?>
-						<a href="<?php echo esc_url( $data['link']['url'] ); ?>" target="<?php echo esc_attr( $data['link']['target'] ); ?>" class="btn btn__white"><?php echo esc_html( $data['link']['title'] ); ?><img src="<?php echo get_template_directory_uri()?>/assets/images/arrow-right.svg" alt="Flèche vers la droite"></a>
+		<div class="col-lg-8 mx-auto">
+			<div id="<?php echo $block_id; ?>" class="<?php echo $class_name; ?>">
+				<div class="card__header">
+
+					<?php if ($data['image']) : ?>
+						<div class="card__icon">
+							<img src="<?php echo esc_url($data['image']['url']); ?>" alt="<?php echo esc_attr($data['image']['alt']); ?>" />
+						</div>
 					<?php endif; ?>
+					<h3 class="title d-sm-none"><span class="g-text"><?php echo $data['title']; ?></span></h3>
+				</div>
+
+				<div class="card__text">
+					<h3 class="title d-none d-sm-flex"><span class="g-text"><?php echo $data['title']; ?></span></h3>
+					<div class="card__text__box mh-auto bg_<?php echo $args['bg_color']; ?>">
+						<h3><?php echo $data['subtitle']; ?></h3>
+						<p><?php echo $data['description']; ?></p>
+						<?php if ($data['link']) : ?>
+							<a href="<?php echo esc_url($data['link']['url']); ?>" target="<?php echo esc_attr($data['link']['target']); ?>" class="btn btn__white"><?php echo esc_html($data['link']['title']); ?><img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow-right.svg" alt="Flèche vers la droite"></a>
+						<?php endif; ?>
+					</div>
 				</div>
 			</div>
+		</div>
 	</div>
 </div>
-	</div>
-</div>
-
-
