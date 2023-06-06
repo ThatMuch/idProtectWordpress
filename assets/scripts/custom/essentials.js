3b1995dc996a4e6fa71c47577bf3bdae9f493766
Mathilde Arconte <signup@marconte.fr>
2023-06-06 17:15:55 +0200

    again

--- assets/scripts/custom/essentials.js
+++ assets/scripts/custom/essentials.js
@@ -104,11 +104,9 @@
 	$('.wp-block-button__link.wp-element-button').each(function () {
 		$(this).removeClass('wp-block-button__link wp-element-button').addClass('btn btn__orange blue');
 	});
-
-
-	$('.wp-block-file').each(function () {
-		// Get the link in this div and add a class
-		$(this).find('a').addClass('btn btn__orange blue');
+	$('.wp-block-file__button').each(function () {
+		$(this).removeClass('wp-block-file__button wp-element-button').addClass('btn btn__orange blue');
+		console.log("first")
 	});
 
 	//==================================================================================
