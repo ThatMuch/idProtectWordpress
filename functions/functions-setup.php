ef1a74cecb9d8faeee28194edd4937fb37c9230f
Mathilde Arconte <signup@marconte.fr>
2024-03-12 00:05:34 +0100

    test

--- functions/functions-setup.php
+++ functions/functions-setup.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The starting point for setting up a new theme.
  * Go through this file to setup your preferences
@@ -51,7 +52,7 @@ $typekit_id = '';
 // set $preload_fonts to false to not preload fonts
 $preload_fonts = true;
 // define font-names and font-formats for all fonts that need preloading (usally the same as in assets/styles/fonts.scss)
-$font_names = ['Raleway-Regular','Raleway-Italic','Raleway-Bold','Rubik-Medium','Rubik-Bold','Rubik-ExtraBold'];
+$font_names = ['Raleway-Regular', 'Raleway-Italic', 'Raleway-Bold', 'Rubik-Medium', 'Rubik-Bold', 'Rubik-ExtraBold'];
 $font_formats = ['ttf'];
 
 
@@ -72,11 +73,11 @@ $GTM_id = '';
 // open graph tags are returned by default => idProtect_ogtags()
 // add your og-image credentials here or leave ['active', false] to not emped an og-image
 $ogimg = [
-  ['active', false],
-  ['path', '/images/ogimg.jpg'],
-  ['height', '300'],
-  ['width', '400'],
-  ['alt', 'true']
+	['active', false],
+	['path', '/images/ogimg.jpg'],
+	['height', '300'],
+	['width', '400'],
+	['alt', 'true']
 ];
 
 
@@ -87,80 +88,90 @@ $ogimg = [
 // loads wordpress-menus, add your custom menus here or remove if not needed
 // by default, only 'mainmenu' is shown
 // => https://codex.wordpress.org/Function_Reference/register_nav_menus
-function stanlee_register_theme_menus() {
-  register_nav_menus([
-    'mainmenu' => __('Mainmenu'),
-    'submenu' => __('Submenu')
-  ]);
+function stanlee_register_theme_menus()
+{
+	register_nav_menus([
+		'mainmenu' => __('Mainmenu'),
+		'submenu' => __('Submenu')
+	]);
 }
-add_action( 'init', 'stanlee_register_theme_menus');
+add_action('init', 'stanlee_register_theme_menus');
 
 
-function stanlee_widgets_init() {
-  register_sidebar( array(
-      'name'          => esc_html__( 'Sidebar par defaut', 'IdProtect' ),
-      'id'            => 'sidebar-default',
-      'description'   => esc_html__( 'Add widgets here.', 'IdProtect' ),
-      'before_widget' => '<aside id="%1$s" class="%2$s">',
-      'after_widget'  => '</aside>',
-      'before_title'  => '<h2>',
-      'after_title'   => '</h2>',
-  ) );
-  register_sidebar( array(
-      'name'          => esc_html__( 'Sidebar particuliers', 'IdProtect' ),
-      'id'            => 'sidebar-particuliers',
-      'description'   => esc_html__( 'Add widgets here.', 'IdProtect' ),
-      'before_widget' => '<aside id="%1$s" class="%2$s article__sidebar sidebar-part">',
-      'after_widget'  => '</aside>',
-      'before_title'  => '<h2>',
-      'after_title'   => '</h2>',
-  ) );
-  register_sidebar( array(
-      'name'          => esc_html__( 'Sidebar professionnels', 'IdProtect' ),
-      'id'            => 'sidebar-professionnels',
-      'description'   => esc_html__( 'Add widgets here.', 'IdProtect' ),
-      'before_widget' => '<aside id="%1$s" class="%2$s article__sidebar sidebar-pro">',
-      'after_widget'  => '</aside>',
-      'before_title'  => '<h2>',
-      'after_title'   => '</h2>',
-  ) );
-  register_sidebar( array(
-      'name'          => esc_html__( 'Footer 1', 'stanlee' ),
-      'id'            => 'footer-1',
-      'description'   => esc_html__( 'Add widgets here.', 'stanlee' ),
-      'before_widget' => '<div id="%1$s" class=" widget %2$s">',
-      'after_widget'  => '</div>',
-      'before_title'  => '<h4 class="widget-title">',
-      'after_title'   => '</h4>',
-  ) );
+function stanlee_widgets_init()
+{
+	register_sidebar(array(
+		'name'          => esc_html__('Sidebar par defaut', 'IdProtect'),
+		'id'            => 'sidebar-default',
+		'description'   => esc_html__('Add widgets here.', 'IdProtect'),
+		'before_widget' => '<aside id="%1$s" class="%2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2>',
+		'after_title'   => '</h2>',
+	));
+	register_sidebar(array(
+		'name'          => esc_html__('Sidebar particuliers', 'IdProtect'),
+		'id'            => 'sidebar-particuliers',
+		'description'   => esc_html__('Add widgets here.', 'IdProtect'),
+		'before_widget' => '<aside id="%1$s" class="%2$s article__sidebar sidebar-part">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2>',
+		'after_title'   => '</h2>',
+	));
+	register_sidebar(array(
+		'name'          => esc_html__('Sidebar professionnels', 'IdProtect'),
+		'id'            => 'sidebar-professionnels',
+		'description'   => esc_html__('Add widgets here.', 'IdProtect'),
+		'before_widget' => '<aside id="%1$s" class="%2$s article__sidebar sidebar-pro">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2>',
+		'after_title'   => '</h2>',
+	));
+	register_sidebar(array(
+		'name'          => esc_html__('Footer 1', 'stanlee'),
+		'id'            => 'footer-1',
+		'description'   => esc_html__('Add widgets here.', 'stanlee'),
+		'before_widget' => '<div id="%1$s" class=" widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h4 class="widget-title">',
+		'after_title'   => '</h4>',
+	));
 }
-add_action( 'widgets_init', 'stanlee_widgets_init' );
+add_action('widgets_init', 'stanlee_widgets_init');
 // Custom search widget
- function my_search_form( $form ) {
-  $form = '<form role="search" method="get" id="searchform" class="search-form" action="' . home_url( '/' ) . '" >
+function my_search_form($form)
+{
+	$form = '<form role="search" method="get" id="searchform" class="search-form" action="' . home_url('/') . '" >
   <div><label>
   <input class="search-field form-control" placeholder="Rechercher" type="text" value="' . get_search_query() . '" name="s" id="s" />
   </label>
   </div>
   </form>';
 
-  return $form;
+	return $form;
 }
 
-add_filter( 'get_search_form', 'my_search_form', 100 );
+add_filter('get_search_form', 'my_search_form', 100);
 
 // Register categories for pages
-function add_categories_to_pages() {
-    register_taxonomy_for_object_type( 'category', 'page' );
+function add_categories_to_pages()
+{
+	register_taxonomy_for_object_type('category', 'page');
 }
-add_action( 'init', 'add_categories_to_pages' );
+add_action('init', 'add_categories_to_pages');
 
 // Ensure that pages are included in category archives
-function display_pages_in_category_archives( $query ) {
-    if ( $query->is_category() && $query->is_main_query() ) {
-        $query->set( 'post_type', array( 'post', 'page' ) );
-    }
+function display_pages_in_category_archives($query)
+{
+	if ($query->is_category() && $query->is_main_query()) {
+		$query->set('post_type', array('post', 'page'));
+	}
 }
-add_action( 'pre_get_posts', 'display_pages_in_category_archives' );
+add_action('pre_get_posts', 'display_pages_in_category_archives');
 
-?>
+
+function mytheme_add_woocommerce_support()
+{
+	add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
