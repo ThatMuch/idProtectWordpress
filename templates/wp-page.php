a5940a9c0ffc00b3409e2135080b2ba20fa1d09c
Mathilde Arconte <signup@marconte.fr>
2024-03-22 22:53:42 +0100

    Mise en page de l'en-tête de la page et du style modifiée

--- templates/wp-page.php
+++ templates/wp-page.php
@@ -10,18 +10,31 @@
 <?php get_header(); ?>
 
 <!-- Page Header Start -->
-<div class="page__header__area" style="background-image: linear-gradient(0deg, rgba(23, 28, 96, 0.5), rgba(23, 28, 96, 0.5)), url('<?php the_post_thumbnail_url(); ?>')">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="page__header">
-					<h1 class="page__header__title"><?php the_title(); ?></h1>
-
+<?php if (has_post_thumbnail($post->ID)) : ?>
+	<div class="page__header__area" style="background-image: linear-gradient(0deg, rgba(23, 28, 96, 0.5), rgba(23, 28, 96, 0.5)), url('<?php the_post_thumbnail_url(); ?>')">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="page__header">
+						<h1 class="page__header__title"><?php the_title(); ?></h1>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+<?php else : ?>
+	<div class="page__header__area--noBg">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="page__header">
+						<h1 class="page__header__title"><?php the_title(); ?></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php endif; ?>
 <!-- Page Header End -->
 <div class="page__area">
 	<div class="container">
