7be4c0858dcc4443a2e251e5c0f2e94958de2970
Mathilde Arconte <signup@marconte.fr>
2024-03-22 22:35:07 +0100

    Ajouter des onglets de produit dans la page de produit unique

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -57,6 +57,7 @@ if (post_password_required()) {
 	$product_excerpt = $product->get_short_description();
 	$product_interval = $product->get_attribute('interval');
 	$product_addToCart = $product->add_to_cart_url();
+	$product_tabs = apply_filters('woocommerce_product_tabs', array());
 	?>
 	<div class="row mb-4">
 		<div class="col-sm-3">
@@ -84,43 +85,34 @@ if (post_password_required()) {
 			</div>
 		</div>
 		<div class="col-md-12">
-			<?php //do_action('woocommerce_after_single_product_summary'); 
-			?>
+			<?php if (!empty($product_tabs)) : ?>
+
+				<div class="woocommerce-tabs wc-tabs-wrapper">
+					<ul class="tabs wc-tabs" role="tablist">
+						<?php foreach ($product_tabs as $key => $product_tab) : ?>
+							<li class="<?php echo esc_attr($key); ?>_tab" id="tab-title-<?php echo esc_attr($key); ?>" role="tab" aria-controls="tab-<?php echo esc_attr($key); ?>">
+								<a href="#tab-<?php echo esc_attr($key); ?>">
+									<?php echo wp_kses_post(apply_filters('woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key)); ?>
+								</a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+					<?php foreach ($product_tabs as $key => $product_tab) : ?>
+						<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr($key); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr($key); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr($key); ?>">
+							<?php
+							if (isset($product_tab['callback'])) {
+								call_user_func($product_tab['callback'], $key, $product_tab);
+							}
+							?>
+						</div>
+					<?php endforeach; ?>
+
+					<?php do_action('woocommerce_product_after_tabs'); ?>
+				</div>
+
+			<?php endif; ?>
 		</div>
 	</div>
-
-
-	<!-- add to cart -->
-
-	<div class="summary entry-summary">
-		<?php
-		/**
-		 * Hook: woocommerce_single_product_summary.
-		 *
-		 * @hooked woocommerce_template_single_title - 5
-		 * @hooked woocommerce_template_single_rating - 10
-		 * @hooked woocommerce_template_single_price - 10
-		 * @hooked woocommerce_template_single_excerpt - 20
-		 * @hooked woocommerce_template_single_add_to_cart - 30
-		 * @hooked woocommerce_template_single_meta - 40
-		 * @hooked woocommerce_template_single_sharing - 50
-		 * @hooked WC_Structured_Data::generate_product_data() - 60
-		 */
-
-		//	do_action('woocommerce_single_product_summary');
-		?>
-	</div>
-	<?php
-	/**
-	 * Hook: woocommerce_after_single_product_summary.
-	 *
-	 * @hooked woocommerce_output_product_data_tabs - 10
-	 * @hooked woocommerce_upsell_display - 15
-	 * @hooked woocommerce_output_related_products - 20
-	 */
-
-
-	?>
 </div>
 
 <?php do_action('woocommerce_after_single_product'); ?>
