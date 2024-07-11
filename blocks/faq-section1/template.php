0fb07f38b13dff9a5a76ee13c4699adf6604f8d5
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-07-11 16:49:30 +0200

    test FAQ

--- blocks/faq-section1/template.php
+++ blocks/faq-section1/template.php
@@ -6,7 +6,6 @@
       <span class="card__title-faq-orange title-faq h1-45-faq mb-5">questions</span>
     
   </div>
-<h1><?= $title ?></h1>
   <p class="b2-medium-faq text-primary">Questions fréquentes : Tout ce que vous devez savoir sur la protection de votre identité avec ID Protect!</p>
     <?php
     $args = array(
@@ -17,15 +16,17 @@
     $faq_query = new WP_Query($args);
     if ($faq_query->have_posts()) :
         $count = 1; // Counter for iteration number
+        var_dump($count);
         while ($faq_query->have_posts()) : $faq_query->the_post(); ?>
             <div class="faq-item">
               <button class="accordion-faq">
-                  <div class="faq-question b2-medium-faq text-primary"><?php the_title() ?></>
+                  <div class="faq-question b2-medium-faq text-primary"><?php the_title() ?></div>
               </button>
               <div class="panel-faq">
                 <div class="faq-answer b2-medium-faq text-primary"><?php the_content() ?></div>
               </div>
             </div>
+            <h1><?= $title ?></h1>
             <?php $count++; ?>
         <?php endwhile;
         wp_reset_postdata();
@@ -34,6 +35,23 @@
     <?php endif; ?>
 </div>
 
-
-
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var acc = document.getElementsByClassName("accordion-faq");
+    var i;
+  
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+  
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+  });
+</script>
 
