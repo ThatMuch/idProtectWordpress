39e691fce42872e4ee3bd2fd00a60fae6a0a3e69
Xal0s <kevinpereira95@free.fr>
2024-07-11 12:08:12 +0200

    modification block avis

--- blocks/cartes_avis/template.php
+++ blocks/cartes_avis/template.php
@@ -1,4 +1,15 @@
 <?php
+// The block attributes
+$block = $args['block'];
+
+// The block data
+$data = $args['data'];
+
+// The block ID
+$block_id = $args['block_id'];
+
+// The block class names
+$class_name = $args['class_name'];
 // Arguments pour la requête des témoignages
 $args = array(
     'post_type' => 'avis',
@@ -6,7 +17,8 @@ $args = array(
 $testimony_query = new WP_Query($args);
 ?>
 
-<section name="Témoignage"> 
+<section name="Avis"> 
+<div id="<?php echo $block_id; ?>" class="<?php echo $class_name; ?>">
     <div class="container">
         <h1 class="h1-45 testimony_title margin-h1">
             <?php the_field('titre'); ?>
@@ -14,7 +26,12 @@ $testimony_query = new WP_Query($args);
         </h1>
         <div id="testimony_box" class="d-flex testimony_box grab">
             <?php if ($testimony_query->have_posts()) : ?>
-                <?php while ($testimony_query->have_posts()) : $testimony_query->the_post(); ?>
+                <?php while ($testimony_query->have_posts()) : $testimony_query->the_post(); 
+                    $note = get_field('note', $post->ID);
+                    $content = get_field('commentaire', $post->ID);
+                    $username = get_field('username', $post->ID);
+                    $sub = get_field('subtitle', $post->ID)
+                ?>
                     <div class="card unselectable">
                         <div class="guillemets">
                             <img src="<?= esc_url(get_template_directory_uri() . '/blocks/cartes_avis/assets/guillemets.svg') ?>" alt="guillemets">
@@ -22,7 +39,6 @@ $testimony_query = new WP_Query($args);
                         <div class="notation">
                             <div class="stars">
                                 <?php 
-                                $note = get_field('note'); // Récupérer le champ 'note' pour ce post
                                 for ($i = 0; $i < $note; $i++) { ?>
                                     <img src="<?= esc_url(get_template_directory_uri() . '/blocks/cartes_avis/assets/star.svg') ?>" alt="star">
                                 <?php } ?>
@@ -32,14 +48,14 @@ $testimony_query = new WP_Query($args);
                             </div>
                         </div>
                         <div class="card_text b3-14_negative">
-                            <p><?= esc_html(get_field('commentaire')); ?></p>
+                            <p><?= $content ?></p>
                         </div>
                         <div class="person">
                             <div class="username">
-                                <p><?php echo esc_html(get_field('username')); ?></p>
+                                <p><?= $username ?></p>
                             </div>
                             <div class="subtitle">
-                                <small><?php echo esc_html(get_field('subtitle')); ?></small>
+                                <small><?= $sub ?></small>
                             </div>
                         </div> 
                     </div>
