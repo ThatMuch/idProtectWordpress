e9803d4fca0c2cef571c5837cfac2b3fe8465ac9
Mathilde Arconte <signup@marconte.fr>
2024-07-25 20:40:01 +0200

    + Avis client

--- blocks/cartes_avis/template.php
+++ blocks/cartes_avis/template.php
@@ -17,16 +17,16 @@ $args = array(
 $testimony_query = new WP_Query($args);
 ?>
 
-<section name="Avis"> 
-<div id="<?php echo $block_id; ?>" class="<?php echo $class_name; ?>">
+
+<section id="<?php echo $block_id; ?>" class="<?php echo $class_name; ?>">
     <div class="container">
-        <h1 class="h1-45 testimony_title margin-h1">
+        <h1 class="testimony_title mb-4">
             <?php the_field('titre'); ?>
-            <span class="orange_title"><?php the_field('titre_accent'); ?></span>
+            <span class="title text__orange"><?php the_field('titre_accent'); ?></span>
         </h1>
         <div id="testimony_box" class="d-flex testimony_box grab">
             <?php if ($testimony_query->have_posts()) : ?>
-                <?php while ($testimony_query->have_posts()) : $testimony_query->the_post(); 
+                <?php while ($testimony_query->have_posts()) : $testimony_query->the_post();
                     $note = get_field('note', $post->ID);
                     $content = get_field('commentaire', $post->ID);
                     $username = get_field('username', $post->ID);
@@ -36,9 +36,9 @@ $testimony_query = new WP_Query($args);
                         <div class="guillemets">
                             <img src="<?= esc_url(get_template_directory_uri() . '/blocks/cartes_avis/assets/guillemets.svg') ?>" alt="guillemets">
                         </div>
-                        <div class="notation">
+                        <div class="d-flex justify-content-between mb-2 text__orange">
                             <div class="stars">
-                                <?php 
+                                <?php
                                 for ($i = 0; $i < $note; $i++) { ?>
                                     <img src="<?= esc_url(get_template_directory_uri() . '/blocks/cartes_avis/assets/star.svg') ?>" alt="star">
                                 <?php } ?>
@@ -47,23 +47,18 @@ $testimony_query = new WP_Query($args);
                                 <span><?= esc_html($note); ?>/5</span>
                             </div>
                         </div>
-                        <div class="card_text b3-14_negative">
+                        <div class="card_text b3-14_negative mb-4">
                             <p><?= $content ?></p>
                         </div>
-                        <div class="person">
-                            <div class="username">
-                                <p><?= $username ?></p>
-                            </div>
-                            <div class="subtitle">
-                                <small><?= $sub ?></small>
-                            </div>
-                        </div> 
+
+                        <p class="mb-2"><?= $username ?></p>
+                        <small class="subtitle"><?= $sub ?></small>
                     </div>
                 <?php endwhile; ?>
                 <?php wp_reset_postdata(); ?>
             <?php else : ?>
                 <p><?php _e('Aucun témoignage trouvé'); ?></p>
             <?php endif; ?>
-        </div>  
+        </div>
     </div>
 </section>
