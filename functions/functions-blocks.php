15c3b5bf42dfa3e2fe855b9d39fdbb0e080ea8c7
Mathilde Arconte <signup@marconte.fr>
2024-08-05 14:02:39 +0200

    chore: Update block name to match French translation

--- functions/functions-blocks.php
+++ functions/functions-blocks.php
@@ -102,7 +102,7 @@ register_block_type(
 );
 
 register_block_type(
-	get_template_directory() . '/blocks/Datas/block.json',
+	get_template_directory() . '/blocks/DatasList/block.json',
 	array(
 		'icon'  => $icon,
 	),
