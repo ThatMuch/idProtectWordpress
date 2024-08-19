950b93e4db48d57a7a87307325ab73ec90f712e9
Mathilde Arconte <signup@marconte.fr>
2024-08-19 17:32:36 +0200

    chore: Update video block styles and template file

--- blocks/Video/template.php
+++ blocks/Video/template.php
@@ -37,9 +37,7 @@ $isReverse = get_field('reverse');
 			<div class="col-lg-7 d-flex h-100">
 				<?php if ($video) : ?>
 					<div class="hero__video mt-auto mb-auto w-100">
-
 						<?= $video; ?>
-
 					</div>
 				<?php endif; ?>
 			</div>
