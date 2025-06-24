2d9642527926a16f4bb2633d2290622dc8b04d01
Mathilde Arconte <signup@marconte.fr>
2025-06-24 11:46:02 +0200

    style: Mise à jour des liens de documentation et des versions dans les fichiers de modèle WooCommerce

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -11,18 +11,18 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see         https://woo.com/document/template-structure/
+ * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     3.9.0
+ * @version     9.6.0
  */
 
-if (!defined('ABSPATH')) {
+if (! defined('ABSPATH')) {
 	exit;
 }
 
 if ($related_products) : ?>
 
-	<section class="related toto products">
+	<section class="related products">
 
 		<?php
 		$heading = apply_filters('woocommerce_product_related_products_heading', __('Related products', 'woocommerce'));
@@ -31,31 +31,18 @@ if ($related_products) : ?>
 		?>
 			<h2><?php echo esc_html($heading); ?></h2>
 		<?php endif; ?>
-
-		<?php woocommerce_product_loop_start();
-		?>
+		<?php woocommerce_product_loop_start(); ?>
 
 		<?php foreach ($related_products as $related_product) : ?>
 
 			<?php
 			$post_object = get_post($related_product->get_id());
 
-			setup_postdata($GLOBALS['post'] = &$post_object); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
+			setup_postdata($GLOBALS['post'] = $post_object); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
 
-			$product_title = $related_product->get_title();
-			$image_id  = $related_product->get_image_id();
-			$image_url = wp_get_attachment_image_url($image_id, 'medium');
-			$product_price = $related_product->get_price();
+			wc_get_template_part('content', 'product');
 			?>
-			<div class="row">
-				<div class="col-sm-2">
-					<img src="<?php echo $image_url ?>" alt="<?php echo $product_title ?>">
-				</div>
-				<div class="col-sm-10">
-					<p><?php echo $product_title ?></p>
-					<p><?php echo $product_price ?></p>
-				</div>
-			</div>
+
 		<?php endforeach; ?>
 
 		<?php woocommerce_product_loop_end(); ?>
