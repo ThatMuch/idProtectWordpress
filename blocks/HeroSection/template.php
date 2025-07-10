bdfe5143fe9895ea0e3be848fe67fac0b6e943b5
Mathilde Arconte <signup@marconte.fr>
2025-07-10 19:11:14 +0200

    Add Hero Section block with customizable fields and styles
    - Created block.json for Hero Section with metadata and ACF settings.
    - Implemented block.php to handle dynamic rendering of the Hero Section.
    - Added style.css for styling the Hero Section with responsive design.
    - Developed template.php to structure the Hero Section layout and display fields.

---
+++ blocks/HeroSection/template.php
@@ -0,0 +1,65 @@
+<?php
+$title = get_field('title');
+$accent = get_field('title_accent');
+$subtitle = get_field('subtitle');
+$image = get_field('image');
+$description = get_field('description');
+$isReverse = get_field('reverse');
+$isFirst = get_field('is_first');
+var_dump($block['id']); // For debugging purposes, remove in production
+?>
+
+<section class="hero__area mb-50 oooooooo">
+	<div class="container">
+		<div class="row align-items-center <?php echo $isReverse ? "flex-row-reverse" : "" ?>">
+			<div class="col-lg-7 h-100">
+				<div class="hero__text">
+					<h1><?php echo $block['id']; ?></h1>
+					<h2><?php echo sg_get_first_block_id(); ?></h2>
+					<?php if ($title) : ?>
+						<?php if ($block['id'] === sg_get_first_block_id()): ?>
+							<h1 class="section__title h1"><?php echo $title; ?> <span class="title text__orange"><?php echo $accent ?></span></h1>
+						<?php else : ?>
+							<h2 class="section__title h1"><?php echo $title; ?> <span class="title text__orange"><?php echo $accent ?></span></h2>
+						<?php endif; ?>
+					<?php endif; ?>
+
+				</div>
+				<div class="hero__box nnnnnn">
+					<?php if ($subtitle) : ?>
+						<?php if ($isFirst) : ?>
+							<h2 class="h2"><?php echo $subtitle; ?></h2>
+						<?php else : ?>
+							<h3 class="h2"><?php echo $subtitle; ?></h3>
+						<?php endif; ?>
+					<?php endif; ?>
+					<?php if ($description) : ?><p><?php echo $description; ?></p><?php endif; ?>
+					<?php if (have_rows('cta_group')) : ?>
+						<div class="hero__box__button">
+							<?php while (have_rows('cta_group')) : the_row();
+								$cta_primary = get_sub_field('primary_cta');
+								$cta_secondary = get_sub_field('secondary_cta');
+							?>
+								<?php if ($cta_primary) : ?>
+									<a class="btn btn__primary" href="<?php echo esc_url($cta_primary['url']); ?>"><span><?php echo esc_html($cta_primary['title']); ?></span></a>
+								<?php endif; ?>
+								<?php if ($cta_secondary) : ?>
+									<a class="btn btn__secondary" href="<?php echo esc_url($cta_secondary['url']); ?>"><span><?php echo esc_html($cta_secondary['title']); ?></span></a>
+								<?php endif; ?>
+							<?php endwhile; ?>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+
+			<div class="col-lg-5 d-flex h-100">
+				<?php if ($image) : ?>
+					<div class="hero__image mt-auto mb-auto">
+						<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+					</div>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+	</div>
+</section>
