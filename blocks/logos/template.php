a8874d049e3b6e075bce985634603ca98a8d2817
Mathilde Arconte <signup@marconte.fr>
2026-08-25 15:05:52 +0200

    fix: reset dropdown menu top position and update logo block styling

--- blocks/logos/template.php
+++ blocks/logos/template.php
@@ -14,31 +14,29 @@ $class_name = $args['class_name'];
 $size = 'medium'; // (thumbnail, medium, large, full or custom size)
 ?>
 <section class="section__logos">
-	<div class="container">
-		<h3 class="title mb-5 h2"><span> <?php echo $data['title']; ?></span></h3>
-		<div id="<?php echo $block_id; ?>" class="<?php echo $class_name; ?> slider">
-			<?php if ($data['gallery']) : ?>
-				<ul class="slide-track">
-					<?php foreach ($data['gallery'] as $image) : ?>
-						<li class="slide">
-							<?php if ($image['description']) : ?>
-								<a href="<?php echo esc_url($image['description']); ?>" target="_blank">
-									<img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-								</a>
-							<?php else : ?>
-								<img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-							<?php endif; ?>
-						</li>
-					<?php endforeach; ?>
-					<?php foreach ($data['gallery'] as $image) : ?>
-						<li class="slide">
+	<h2 class="section__logos__title"><span> <?php echo $data['title']; ?></span></h2>
+	<div id="<?php echo $block_id; ?>" class="<?php echo $class_name; ?> slider">
+		<?php if ($data['gallery']) : ?>
+			<ul class="slide-track">
+				<?php foreach ($data['gallery'] as $image) : ?>
+					<li class="slide">
+						<?php if ($image['description']) : ?>
 							<a href="<?php echo esc_url($image['description']); ?>" target="_blank">
 								<img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 							</a>
-						</li>
-					<?php endforeach; ?>
-				</ul>
-			<?php endif; ?>
-		</div>
+						<?php else : ?>
+							<img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+						<?php endif; ?>
+					</li>
+				<?php endforeach; ?>
+				<?php foreach ($data['gallery'] as $image) : ?>
+					<li class="slide">
+						<a href="<?php echo esc_url($image['description']); ?>" target="_blank">
+							<img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+						</a>
+					</li>
+				<?php endforeach; ?>
+			</ul>
+		<?php endif; ?>
 	</div>
 </section>
